b2388ee4e67e35c28240e54b30f5edbc1658f144
dspec12 <danspec12@gmail.com>
2018-11-30 13:52:25 -0500

    7-2 complete

--- part1/chp07/7-2.py
+++ part1/chp07/7-2.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+
+'''
+Figure out what the result will be (True or False) when evaluating the following expressions, 
+then type them into the interactive window to check your answers:
+
+ (1 <= 1) and (1 != 1)
+ not (1 != 2)
+ ("good" != "bad") or False
+ ("good" != "Good") and not (1 == 1)
+ '''
+
+print("(1 <= 1) and (1 != 1)")
+print("True and False")
+print("A: False")
+print()
+print("not (1 != 2)")
+print("not True")
+print("A: False")
+print()
+print('("good" != "bad") or False')
+print("(True) or False")
+print("A: True")
+print()
+print('("good" != "Good") and not (1 == 1)')
+print("True and not (True)")
+print("True and False")
+print("A: False")
