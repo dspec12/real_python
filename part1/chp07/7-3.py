2ac4aca40592f39c1add9e1807ba6e8f8d9461db
dspec12 <danspec12@gmail.com>
2018-11-30 14:19:26 -0500

    7-3 complete

--- part1/chp07/7-3.py
+++ part1/chp07/7-3.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
+'''
+Write a script that prompts the user to enter a word using the input() function, stores that input in a variable, 
+and then displays whether the length of that string is less than 5 characters, greater than 5 characters, or equal 
+to 5 characters by using a set of if, elif and else statements.”
+'''
 
+name = input("What's your name? ")
+
+if len(name) < 5:
+    print("Your name is less than five characters.")
+
+elif len(name) > 5:
+    print("Your name is greater than five characters.")
+
+else:
+    print("Your name is equal to five characters")
