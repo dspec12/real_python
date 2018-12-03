506a8607ae8fa61ccb43fd5dab2f6b6311aec1a4
dspec12 <danspec12@gmail.com>
2018-12-03 13:54:00 -0500

    elections.py complete

--- part1/chp07/election.py
+++ part1/chp07/election.py
@@ -16,4 +16,33 @@ that Candidate B will win.
 
 Hint: To do this, you'll probably need to use a for loop with a lot of if/else statements to check the results of each regional election.
 '''
+from random import random
+
+total_A_wins = 0
+total_B_wins = 0
+
+trials = 100000
+for trial in range(0, trials):
+    A_win = 0
+    B_win = 0
+    if random() < .87:  # 1st region
+        A_win += 1
+    else:
+        B_win += 1
+    if random() < .65:  # 2nd region
+        A_win += 1
+    else:
+        B_win += 1
+    if random() < .17:  # 3rd region
+        A_win += 1
+    else:
+        B_win += 1
+    # determine overall election outcome
+    if A_win > B_win:
+        total_A_wins += 1
+    else:
+        total_B_wins += 1
+
+print("Probability A wins:", total_A_wins / trials)
+print("Probability B wins:", total_B_wins / trials)
 
