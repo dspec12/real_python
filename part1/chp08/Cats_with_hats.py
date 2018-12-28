3ad754781554592293bc2bced5fb4239ec20c8a1
dspec12 <danspec12@gmail.com>
2018-12-28 11:24:55 -0500

    cats with hats complete

---
+++ part1/chp08/Cats_with_hats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''
+You have 100 cats.
+
+One day you decide to arrange all your cats in a giant circle. Initially, none of your cats have any hats on. You walk around the circle 100 times, 
+always starting at the same spot, with the first cat (cat # 1). Every time you stop at a cat, you either put a hat on it if it doesn't have one on, 
+or you take its hat off if it has one on.
+
+The first round, you stop at every cat, placing a hat on each one.
+The second round, you only stop at every second cat (#2, #4, #6, #8, etc.).
+The third round, you only stop at every third cat (#3, #6, #9, #12, etc.).
+You continue this process until you've made 100 rounds around the cats (e.g., you only visit the 100th cat).
+Write a program that simply outputs which cats have hats at the end
+'''
+
+CWH = []
+CWNH = []
+
+def place_hat(cat):
+    '''Takes or removes hat from a cat's head'''
+    if cat in CWH:
+         CWH.remove(cat)
+    else:
+        CWH.append(cat)
+        
+def cat_no_hat(cat):
+    '''Returns all cats without hats'''
+    if cat in CWH:
+        pass
+    else:
+        CWNH.append(cat)
+        
+
+for round in range(1,101):
+    for cat in range(1,101):
+        if cat % round:
+            pass
+        else:
+            place_hat(cat)
+
+for cat in range(1,101):
+    cat_no_hat(cat)
+
+print("Total number of cat with hats: ", len(CWH))
+print("Specific cats with hats: ", CWH)
+print()
+print("Total number of cat with no hats: ", len(CWNH))
+print("Specific cats without hats: ", CWNH)
